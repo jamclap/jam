1b22a5c0719608b40107feba4edc85b5d20586f6
Tom <tom@contextfree.info>
2025-11-22 11:40:55 -0700

    Try eval path

--- jam/run/run.go
+++ jam/run/run.go
@@ -1,9 +1,19 @@
 package run
 
-import "github.com/traefik/yaegi/interp"
+import (
+	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
+)
 
 func Run(script string) error {
 	i := interp.New(interp.Options{})
 	_, err := i.Eval(script)
+	return err
+}
+
+func RunPath(path string) error {
+	i := interp.New(interp.Options{})
+	i.Use(stdlib.Symbols)
+	_, err := i.EvalPath(path)
 	return err
 }
--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/jamclap/jam/jam/run"
 )
 
 func main() {
-	err := run.Run(`
-package main
+	if len(os.Args) < 2 {
+		return
+	}
+	path := os.Args[1]
+	err := run.RunPath(path)
+	// b, err := os.ReadFile(path)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// source := string(b)
+	// 	err := run.Run(`
+	// package main
 
-func main() {
-	println("Hi!")
-}
-	`)
+	// func main() {
+	// 	println("Hi!")
+	// }
+	// 	`)
 	if err != nil {
 		log.Panic(err)
 	}
